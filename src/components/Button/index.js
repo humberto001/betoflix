1888ed8592a00abba54ec8dcac4439dae9cc6822
Humberto Neves <hneves@uolinc.com>
2020-08-03 01:35:09 -0300

    Class 4- effect, json-server

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,11 +1,12 @@
-import styled from "styled-components"
+import styled from 'styled-components';
 
 const Button = styled.button`
 color: var(--white);
     border: 1px solid var(--white);
+    background: var(--black);
     box-sizing: border-box;
     cursor: pointer;
-    padding: 16px 24px;
+    padding: 16px 24px; 
     font-style: normal;
     font-weight: bold;
     font-size: 16px;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,32 +1,111 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
+import FormField from '../../../components/FormField';
+import Button from '../../../components/Button';
 
 function CadastroCategoria() {
-    return (
-        <PageDefault>
-            <h1>Cadastro de Categoria</h1>
+  const [categories, setCategories] = useState([]);
+  const categoryStartValues = {
+    name: '',
+    color: '',
+    description: '',
+  };
+  const [categoryValues, setCategoryValues] = useState(categoryStartValues);
 
-            <form>
+  function setValue(key, value) {
+    setCategoryValues({
+      ...categoryValues,
+      [key]: value,
+    });
+  }
 
-                <label>
-                    Nome da Categoria:
-            <input
-                        type="text"
-                    />
-                </label>
+  function handleChange(info) {
+    // const { getAttribute, value } = info.target; Didn't work!
+    setValue(
+      info.target.getAttribute('name'),
+      info.target.value,
+    );
+  }
 
-                <button>
-                    Cadastrar
-          </button>
-            </form>
+  useEffect(() => {
+    const URL = window.location.hostname.includes('localhost')
+      ? 'http://localhost:8080/categories'
+      : 'https://en.com/categorias';
 
+    fetch(URL).then(async (response) => {
+      const result = await response.json();
+      setCategories([...result]);
+    });
+  }, []);
 
-            <Link to="/">
-                Ir para home
-        </Link>
-        </PageDefault>
-    )
+  return (
+    <PageDefault>
+      <h1>
+        Cadastro de Categoria:
+        {categoryValues.name}
+      </h1>
+
+      <form onSubmit={function handleSubmit(infos) {
+        infos.preventDefault();
+        setCategories([
+          ...categories, categoryValues,
+        ]);
+        setCategoryValues(categoryStartValues);
+      }}
+      >
+
+        <FormField
+          label="Nome da Categoria :"
+          type="text"
+          name="name"
+          value={categoryValues.name}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Cor :"
+          type="color"
+          name="color"
+          value={categoryValues.color}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição :"
+          type="textarea"
+          name="description"
+          value={categoryValues.description}
+          onChange={handleChange}
+        />
+
+        <Button>
+          Cadastrar
+        </Button>
+
+        {categories.length === 0 && (
+        <div>
+          Loading...
+        </div>
+        )}
+      </form>
+
+      <ul>
+        {categories.map((category, index) => (
+
+          // eslint-disable-next-line react/no-array-index-key
+          <li key={`${category}${index}`}>
+            {category.name}
+          </li>
+        ))}
+
+      </ul>
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  );
 }
 
 export default CadastroCategoria;
